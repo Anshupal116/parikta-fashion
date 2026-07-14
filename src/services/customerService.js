d9e978f3a06d43985cada7d6665f123e70112139
Anshupal <palanshu793@gmail.com>
2026-07-14 12:47:10 +0530

    Add admin customers API

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-
+const API_URL = import.meta.env.VITE_API_URL;
 export const getCustomers = async () => {
   const response = await axios.get(
     `${API_URL}/customers/admin/all`

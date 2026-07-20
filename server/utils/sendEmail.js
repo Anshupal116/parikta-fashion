d2fd5c7d2d155acd5db537ece4e6694c2ed09613
Anshupal <palanshu793@gmail.com>
2026-07-20 12:33:58 +0530

    email2

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,10 +4,17 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  family: 4,
+
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 30000,
 });
 
 const sendEmail = async ({ to, subject, html }) => {

6415aed919e918f22ca7acc04efa190e25c1564e
Anshupal <palanshu793@gmail.com>
2026-07-20 12:50:33 +0530

    force IPv4 first for email SMTP

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,13 +1,21 @@
+const dns = require("node:dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 60000,
 });
 
 const sendEmail = async ({ to, subject, html }) => {

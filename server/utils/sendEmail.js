48204d97dc267dbca590acd443d0f5797065ff01
Anshupal <palanshu793@gmail.com>
2026-07-20 14:31:50 +0530

    Use Resend Email Service

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,24 +3,15 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  if (!to) {
-    throw new Error("Receiver email is required");
-  }
-
   const { data, error } = await resend.emails.send({
-    from:
-      process.env.EMAIL_FROM ||
-      "Parikta Fashion <onboarding@resend.dev>",
-
+    from: process.env.EMAIL_FROM,
     to: [to],
-
     subject,
-
     html,
   });
 
   if (error) {
-    console.error(error);
+    console.error("Resend Error:", error);
     throw new Error(error.message);
   }
 
